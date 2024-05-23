638932dfbbaf0ff15ff15974d537c38cb94ae9b7
magotte <gatziaris.pantelis@gmail.com>
2024-05-23 16:47:53 +0300

    feat PF-1:  Fetching Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
+  selector: 'pf-root',
   standalone: true,
   imports: [RouterOutlet],
+  providers: [HttpClient],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'pf-angular-dashboard';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CryptoDashComponent } from '@pf-app/components';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+  path: '',
+  pathMatch: 'full',
+  redirectTo: 'crypto-dashboard'
+}, {
+  path: 'crypto-dashboard',
+  component: CryptoDashComponent
+}, {
+  path: '**',
+  redirectTo: 'crypto-dashboard'
+}];
